f77c4a48e284949ff26d371b521d32cfc94b0c1e
Pascal Lim <pascal.lim@polka.mu>
2017-06-03 03:19:56 +0400

    Fix

--- src/Form/MailgunSettingsForm.php
+++ src/Form/MailgunSettingsForm.php
@@ -7,18 +7,18 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
 
 /**
- * Class MailgunAdminSettingsForm.
+ * Class MailgunSettingsForm.
  *
  * @package Drupal\mailgun\Form
  */
-class MailgunAdminSettingsForm extends ConfigFormBase {
+class MailgunSettingsForm extends ConfigFormBase {
 
   /**
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
     return [
-      'mailgun.adminsettings',
+      'mailgun.settings',
     ];
   }
 
@@ -26,50 +26,50 @@ class MailgunAdminSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function getFormId() {
-    return 'mailgun_admin_settings_form';
+    return 'mailgun_settings_form';
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    
-    $config = $this->config('mailgun.adminsettings');
+
+    $config = $this->config('mailgun.settings');
 
     $url = Url::fromUri('https://mailgun.com/app/domains');
     $link = \Drupal::l(t('mailgun.com/app/domains'), $url);
-    
+
     $form['description'] =
       [
         '#markup' => "Please refer to $link for your settings."
       ];
 
-    $form['api_key'] = array(
+    $form['api_key'] = [
       '#title' => t('Mailgun API Key'),
       '#type' => 'textfield',
       '#description' => t('Enter your API key.'),
       '#default_value' => $config->get('api_key'),
-    );
+    ]
 
-    $form['api_endpoint'] = array(
+    $form['api_endpoint'] = [
       '#title' => t('Mailgun API Endpoint'),
       '#type' => 'textfield',
       '#description' => t('Enter your API endpoint.'),
       '#default_value' => $config->get('api_endpoint'),
-    );
+    ];
 
-    $form['working_domain'] = array(
+    $form['working_domain'] = [
       '#title' => t('Mailgun API Working Domain'),
       '#type' => 'textfield',
       '#description' => t('Enter your API working domain.'),
       '#default_value' => $config->get('working_domain'),
-    );
+    ]
 
-    $form['debug_mode'] = array(
+    $form['debug_mode'] = [
       '#title' => t('Enable Debug Mode'),
       '#type' => 'checkbox',
       '#default_value' => $config->get('debug_mode'),
-    );
+    ];
 
     return parent::buildForm($form, $form_state);
   }
@@ -86,7 +86,7 @@ class MailgunAdminSettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
 
-    $this->config('mailgun.adminsettings')
+    $this->config('mailgun.settings')
       ->set('api_key', $form_state->getValue('api_key'))
       ->set('api_endpoint', $form_state->getValue('api_endpoint'))
       ->set('working_domain', $form_state->getValue('working_domain'))
